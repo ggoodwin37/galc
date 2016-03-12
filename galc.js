3a20cf4e1e92cb5f6d2c8ce64ef3cff0227d6bfe
gideong <gideong@yahoo-inc.com>
2016-03-11 21:47:48 -0800

    Key handlers.

--- galc.js
+++ galc.js
@@ -1,3 +1,11 @@
+
+
+
 function main() {
-    console.log('TODO: write code here.');
+    // attach handlers
+    $('.calc-main').on('click', '.calc-button', function(ev) {
+        var buttonKey = $(ev.target).attr('data-button-key');
+        console.log('button click: ', buttonKey);
+    });
+
 }
